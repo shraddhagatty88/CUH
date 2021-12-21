27f5bc339d978ee528006b3102346b5512d24588
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-12-21 09:09:54 +0000

    uncommented output file

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 ############################################################################
 # Outputs:
 ############################################################################
-/*
+
 output "common_services_compartment_id" {
   value = module.iam.compartments["common_services"]
 }
@@ -10,41 +10,14 @@ output "prod_services_compartment_id" {
   value = module.iam.compartments["prod_services"]
 }
 
-output "nprd_services_compartment_id" {
-  value = module.iam.compartments["nprd_services"]
-}
 
 ############################################################################
 
 output "sub_dmz_id" {
-  value = module.vcn.subnets["gmp_vcn_sub_dmz"]
+  value = module.vcn.subnets["vcn1_sub_dmz"]
 }
 
 output "sub_app_id" {
-  value = module.vcn.subnets["gmp_vcn_sub_app"]
+  value = module.vcn.subnets["vcn1_sub_private"]
 }
-
-output "sub_db_id" {
-  value = module.vcn.subnets["gmp_vcn_sub_db"]
-}
-
-output "sub_lbint_id" {
-  value = module.vcn.subnets["gmp_vcn_sub_lbint"]
-}
-
-output "sub_fss_id" {
-  value = module.vcn.subnets["gmp_vcn_sub_fss"]
-}
-
 ################################################################################
-
-output "nprd_mount_target" {
-  value = oci_file_storage_mount_target.GMP_nprd_mount_target.id
-}
-output "prod_mount_target" {
-  value = oci_file_storage_mount_target.GMP_prod_mount_target.id
-}
-output "dr_mount_target" {
-  value = oci_file_storage_mount_target.GMP_dr_mount_target.id
-}
-*/
